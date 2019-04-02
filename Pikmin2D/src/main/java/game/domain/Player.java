6ea7b07e4ddceb571f4db3f3c87fc421b89d7f55
JustAGoldeneye <eemeli.leppa@gmail.com>
2019-04-02 18:17:40 +0300

    Player can now move

--- Pikmin2D/src/main/java/game/domain/Player.java
+++ Pikmin2D/src/main/java/game/domain/Player.java
@@ -3,18 +3,64 @@ package game.domain;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
-//import javafx.geometry.Point2D;
+import javafx.geometry.Point2D;
 
 public class Player {
     private Circle player;
+    private Point2D movement;
+    private int rotationModifier;
+    private double accelerationModifier;
+    private double maxAcceleration;
     
-    public Player(double xPos, double yPos, double radius, String imageFileName) {
+    public Player(double xPos, double yPos, double rotation, double radius, String imageFileName) {
         this.player = new Circle(xPos, yPos, radius);
+        
         Image image = new Image(imageFileName,false);
         this.player.setFill(new ImagePattern(image));
+        
+        this.player.setRotate(rotation);
+        this.movement = new Point2D(0, 0);
+        
+        //Modifiers
+        this.rotationModifier = 3;
+        this.accelerationModifier = 0.4;
+        this.maxAcceleration = 0.6;
     }
 
     public Circle getPlayer() {
         return player;
+    }
+    
+    public void turnLeft() {
+        this.player.setRotate(this.player.getRotate() - this.rotationModifier);
+    }
+    
+    public void turnRight() {
+        this.player.setRotate(this.player.getRotate() + this.rotationModifier);
+    }
+    
+    public void accelerate() {
+        double changeX = Math.cos(Math.toRadians(this.player.getRotate()));
+        double changeY = Math.sin(Math.toRadians(this.player.getRotate()));
+        
+        changeX *= this.accelerationModifier;
+        changeY *= this.accelerationModifier;
+        
+        Point2D temp = new Point2D(this.movement.getX(), this.movement.getY());
+        
+        if (this.movement.magnitude() <= this.maxAcceleration) {
+            this.movement = this.movement.add(changeX, changeY);
+        } else {
+            this.movement = temp;
+        }
+    }
+    
+    public void stop() {
+        this.movement = this.movement.multiply(0);
+    }
+    
+    public void move() {
+        this.player.setTranslateX(this.player.getTranslateX() + this.movement.getX());
+        this.player.setTranslateY(this.player.getTranslateY() + this.movement.getY());
     }
 }
--- Pikmin2D/src/main/java/game/ui/GameArea.java
+++ Pikmin2D/src/main/java/game/ui/GameArea.java
@@ -3,10 +3,14 @@ package game.ui;
 import game.domain.Player;
 import java.util.Scanner;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 
 public class GameArea extends Application {
     static double paneWidth;
@@ -28,6 +32,34 @@ public class GameArea extends Application {
         stage.setScene(scene);
         stage.setTitle("Pikmin 2D");
         stage.show();
+        
+        Map<KeyCode, Boolean> pressedButtons = new HashMap<>();
+        
+        scene.setOnKeyPressed(event -> {
+           pressedButtons.put(event.getCode(), Boolean.TRUE);
+        });
+        scene.setOnKeyReleased(event -> {
+           pressedButtons.put(event.getCode(), Boolean.FALSE);
+           player.stop();
+        });
+        
+        new AnimationTimer() {
+            
+            @Override
+            public void handle(long current) {
+                if (pressedButtons.getOrDefault(KeyCode.A, false)) {
+                    player.turnLeft();
+                }
+                if (pressedButtons.getOrDefault(KeyCode.D, false)) {
+                    player.turnRight();
+                }
+                if (pressedButtons.getOrDefault(KeyCode.W, false)) {
+                    player.accelerate();
+                }
+                
+                player.move();
+            }
+        }.start();
     }
     
     public void importMapInfo(String fileName) {
@@ -45,7 +77,7 @@ public class GameArea extends Application {
                     paneHeight = Double.valueOf(rowData[1]);
                     
                 } else if (rowData[0].equals("Player")) {
-                    player = new Player(Double.valueOf(rowData[1]), Double.valueOf(rowData[2]), Double.valueOf(rowData[3]), rowData[4]);
+                    player = new Player(Double.valueOf(rowData[1]), Double.valueOf(rowData[2]), Double.valueOf(rowData[3]), Double.valueOf(rowData[4]), rowData[5]);
                 } else {
                     System.out.println("Error: Row " + row + " in the map info file cannot be read. The object wasn't loaded.");
                 }
