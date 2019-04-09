b08f64813d8b0ae2e871cde618fc958bf1deacb3
JustAGoldeneye <eemeli.leppa@gmail.com>
2019-04-09 21:24:02 +0300

    Edited testmap1, made junit for Player

--- Pikmin2D/src/test/java/PlayerTest.java
+++ Pikmin2D/src/test/java/PlayerTest.java
@@ -1,17 +1,57 @@
+
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import game.domain.Player;
+import game.domain.Pikmin.PikminType;
+
 public class PlayerTest {
-    
-    public PlayerTest() {
-    }
+    Player player;
     
     @Before
     public void setUp() {
+        player = new Player();
     }
     
     @Test
-    public void hello() {
+    public void arrayIsSizeOfEnum() {
+        assertTrue(player.getPikmins().length == PikminType.values().length);
+    }
+    
+    @Test
+    public void arrayInitiallyFilledWithZeros() {
+        for (int i = 0; i < player.getPikmins().length; i++) {
+            if (this.player.getPikmins()[i] != 0) {
+                fail("There was something else than 0 in the array.");
+            }
+        }
+    }
+    
+    @Test
+    public void addingPikminsWorks1() {
+        player.addPikmin(PikminType.RED);
+    }
+    
+    @Test
+    public void addingPikminsWorks2() {
+        for (int i = 1; i <= 80; i++) {
+            player.addPikmin(PikminType.YELLOW);
+        }
+        assertTrue(player.getPikmins()[1] == 80);    
+    }
+    
+    @Test
+    public void addingPikminsWorks3() {
+        for (int i = 1; i <= 42; i++) {
+            player.addPikmin(PikminType.RED);
+        }
+        for (int i = 1; i <= 12; i++) {
+            player.addPikmin(PikminType.YELLOW);
+        }
+        for (int i = 1; i <= 55; i++) {
+            player.addPikmin(PikminType.BLUE);
+        }
+        assertTrue(player.getPikmins()[0] == 42 && player.getPikmins()[1] == 12 && player.getPikmins()[2] == 55);
     }
 }
---
+++ Pikmin2D/src/test/java/RedPikminTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import game.domain.Pikmin.RedPikmin;
+
+public class RedPikminTest {
+    
+    @Before
+    public void setUp() {
+    }
+    
+}
